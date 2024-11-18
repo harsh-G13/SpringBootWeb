abf6274e37d1d21e65129587477d95eca76714a0
Harsh Goyal <harsh.goyal.cd.eee20@itbhu.ac.in>
2024-11-18 13:27:07 +0530

    Unused imports removed

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.Serial;
+
 
 
 public class HelloServlet extends HttpServlet {
